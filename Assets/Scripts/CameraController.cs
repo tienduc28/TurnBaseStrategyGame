1f57cab2c88fbc1298a9c250282ea6fc2d424b8a
TienDuc <tienduc2803@gmail.com>
2025-01-07 02:23:50 +0700

    Update CameraController
    Add Rotation by using Q and E for Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private Vector3 targetFollowOffSet;
+
+    private CinemachineTransposer cinemachineTransposer;
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffSet = cinemachineTransposer.m_FollowOffset;
+    }
     private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
     {
         Vector3 inputMoveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
@@ -26,12 +46,14 @@ public class CameraController : MonoBehaviour
 
         float moveSpeed = 10f;
         Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
-        
+
         transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
 
-
+    private void HandleRotation()
+    {
         Vector3 rotationVector = new Vector3(0, 0, 0);
-        float rotationSpeed = 100f;
+
         if (Input.GetKey(KeyCode.E))
         {
             rotationVector.y = +1f;
@@ -41,8 +63,29 @@ public class CameraController : MonoBehaviour
             rotationVector.y = -1f;
         }
 
+        float rotationSpeed = 100f;
         transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
 
-        Debug.Log(Input.mouseScrollDelta);
+    private void HandleZoom()
+    {
+        float zoomAmount = 1f;
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            targetFollowOffSet.y -= zoomAmount;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFollowOffSet.y += zoomAmount;
+        }
+
+        targetFollowOffSet.y = Mathf.Clamp(targetFollowOffSet.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+
+        // Smooth the camera when zoom in and out
+        float zoomSpeed = 5f;
+        cinemachineTransposer.m_FollowOffset =
+               Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffSet, Time.deltaTime * zoomSpeed);
     }
 }
